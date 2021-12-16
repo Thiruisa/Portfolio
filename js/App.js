8fdfdef35426a4e727e9bdd4181df3443c35c150
Thiruisa Puvanenthirarasa <thiruisa@hotmail.co.uk>
2021-12-16 17:09:46 +0000

    update

--- js/App.js
+++ js/App.js
@@ -30,3 +30,20 @@ navLinks.forEach(link => {
         })
     })
 })
+
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
